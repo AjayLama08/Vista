579b456f65bf2456192eb2ed005349c66781cbdb
q2043246 <q2043246@tees.ac.uk>
2024-10-10 12:49:47 +0100

    Completed

--- Vista.App/Program.cs
+++ Vista.App/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Vista.App.Data; //Import the database classes
+
+var trainerDbContext = new TrainersDbContext();  //Create database context
+
+ListCategories(trainerDbContext);
+
+//Method to list categories from the database
+static void ListCategories(TrainersDbContext dbContext)
+{
+    //Load list of categories form the database
+    var categoryList = dbContext.Categories.ToList();
+    Console.WriteLine("Category List\n");
+
+    //Display list of categories from the memory object (categoryList)
+    foreach(var cat in categoryList){
+        Console.WriteLine($"{cat.CategoryCode} {cat.CategoryName}");
+    }
+    Console.WriteLine();
+}
