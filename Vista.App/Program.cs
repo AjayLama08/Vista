d84d5a18d150c01f4512bd1f3a0dae676ca02d63
q2043246 <q2043246@tees.ac.uk>
2024-10-10 12:58:03 +0100

    Successfully Completed

--- Vista.App/Program.cs
+++ Vista.App/Program.cs
@@ -3,6 +3,7 @@
 var trainerDbContext = new TrainersDbContext();  //Create database context
 
 ListCategories(trainerDbContext);
+ListTrainers(trainerDbContext);
 
 //Method to list categories from the database
 static void ListCategories(TrainersDbContext dbContext)
@@ -12,8 +13,24 @@ static void ListCategories(TrainersDbContext dbContext)
     Console.WriteLine("Category List\n");
 
     //Display list of categories from the memory object (categoryList)
-    foreach(var cat in categoryList){
+    foreach (var cat in categoryList)
+    {
         Console.WriteLine($"{cat.CategoryCode} {cat.CategoryName}");
     }
     Console.WriteLine();
+}
+
+    //Method to list trainers from the database
+    static void ListTrainers(TrainersDbContext dbContext)
+{
+    //Load list of categories from the database
+    var trainerList = dbContext.Trainers.ToList();
+    Console.WriteLine("Trainer List\n");
+
+    //Display list of categories from the memory object (trainerList)
+    foreach (var t in trainerList)
+    {
+        Console.WriteLine($"{t.TrainerId} {t.Name}");
+    }
+        Console.WriteLine();
 }
